0d71e08beb70c93027501802e715ef6301806cd5
Colin Rabyniuk <colinxr@gmail.com>
2017-07-29 12:14:42 -0400

    opening up multiple Google CSE queries to get total number of links

--- app.js
+++ app.js
@@ -10,30 +10,71 @@ const cheerio    = require('cheerio');
 require('dotenv').config();
 
 let urls = [];
+let queryNum;
 
 let options = {
-  uri: 'https://www.googleapis.com/customsearch/v1?q=cohousing&cx=006186647395206535033%3Ayn35ydj3ohe&key=' + process.env.KEY,
+  uri: 'https://www.googleapis.com/customsearch/v1?q=cohousing&cx=006186647395206535033%3Ayn35ydj3ohe',
+  qs: {
+    'key': process.env.KEY,
+    'start': 1
+  },
   headers: {
     'User-Agent': 'Request-Promise'
   },
     json: true
 };
 
+
+let getUrls = (arr) => {
+  arr.forEach((res, i) => {
+    let link = arr[i].link;
+
+    return urls.push(link);
+  });
+}
+
 rp(options)
   .then(data => {
+    let results = data.searchInformation.totalResults; // gets total number of results
+    let queryNum = Math.floor(results / data.items.length); // finds number of queries we'll need to use to get all of the results. Math.floor is used because we've already done one query.
+    let queryVars = {
+      options: options,
+      queryNum: queryNum
+    } // sets queryVars object in order to pass down the promise chain.
+
     let arr = data.items;
+    getUrls(arr); // push data.
 
-    arr.forEach(function (res, i){
-      let link = arr[i].link;
+    return queryVars;
+  })
+  .then(function(queryVars){
+    let queries = [];
 
-      urls.push(link);
+    let start = 11;
+
+    let queryOptions = queryVars.options;
+    let queryNum = queryVars.queryNum;
+
+    for (var i = 0; i < queryNum; i++ ){
+
+      queryOptions.qs.start = start;
+      start += 10;
+
+      queries.push(rp(queryOptions));
+    }
+
+    console.log(queries);
+
+  /*  Promise.all(queries)
+    .then((data) => {
+      getUrls(arr);
+      console.log(urls.length);
     })
+    .catch(err => console.log(err));  // First rejected promise*/
   })
   .then(function(){
 
-    console.log(urls);
-
-    urls.forEach(function(url, i){
+    urls.forEach((url, i) => {
       let options = {
         uri: urls[i],
         simple: false,
@@ -59,7 +100,7 @@ rp(options)
           details.lat = $('#map').attr('data-latitude');
           details.long = $('#map').attr('data-longitude');
 
-          console.log(details);
+          //console.log(details);
         }
 
       })
